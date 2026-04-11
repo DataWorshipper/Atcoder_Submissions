1b978bce63aa5f53acf61621423e99c6f0b84497
Abhiraj Mandal <mandalabhiraj04@gmail.com>
2026-04-11 17:32:00 +0530

    Add solution for problem `A - Trimo` Link: https://atcoder.jp/contests/abc453/tasks/abc453_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc453/submissions/74832665

---
+++ submissions/atcoder/abc453/abc453A Trimo.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define v vector
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define f(arr, n) for(ll i = 0; i < n; i++) cin >> arr[i];
+ 
+ 
+ 
+vector<ll> sieve(ll sieve_size)
+{
+    vector<ll> s(sieve_size + 1, 1);
+    s[0] = s[1] = 0;  
+ 
+    for (ll i = 2; i * i <= sieve_size; i++)
+    {
+        if (s[i] == 1)
+        {
+            for (ll j = i * i; j <= sieve_size; j += i)  
+            {
+                s[j] = 0;
+            }
+        }
+    }
+    return s;
+}
+ 
+int main()
+{   
+    fast_io;
+    ll n;
+    
+    string s;
+    cin>>n;
+    cin >> s;
+
+    int i = 0;
+    while(i < s.size() && s[i] == 'o')
+        i++;
+
+    cout << s.substr(i) <<endl;
+}
