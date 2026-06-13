24d406066e7600845d1ce1e0f68263ff77620202
Abhiraj Mandal <mandalabhiraj04@gmail.com>
2026-06-13 17:36:00 +0530

    Add solution for problem `B - Gift` Link: https://atcoder.jp/contests/abc462/tasks/abc462_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc462/submissions/76624244

---
+++ submissions/atcoder/abc462/abc462B Gift.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define v vector
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define f(arr, n) for(ll i = 0; i < n; i++) cin >> arr[i];
+
+const ll MOD = 998244353;
+const ll MAXD = 1000005;
+
+vector<ll> sieve(ll sieve_size)
+{
+    vector<ll> s(sieve_size + 1, 1);
+    s[0] = s[1] = 0;
+
+    for (ll i = 2; i * i <= sieve_size; i++)
+    {
+        if (s[i])
+        {
+            for (ll j = i * i; j <= sieve_size; j += i)
+            {
+                s[j] = 0;
+            }
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    fast_io;
+
+    ll n;
+    cin >> n;
+
+    v<v<ll>> adj(n + 1);
+
+    for (ll i = 1; i <= n; i++)
+    {
+        ll k;
+        cin >> k;
+
+        while (k--)
+        {
+            ll x;
+            cin >> x;
+            adj[x].pb(i);
+        }
+    }
+
+    for (ll i = 1; i <= n; i++)
+    {
+        cout << adj[i].size();
+
+        for (auto x : adj[i])
+            cout << " " << x;
+
+        cout << '\n';
+    }
+
+    return 0;
+}
