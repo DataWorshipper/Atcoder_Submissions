eb002e157ad0d4188490572fbcf966d0bb28b904
Abhiraj Mandal <mandalabhiraj04@gmail.com>
2026-06-13 18:04:00 +0530

    Add solution for problem `C - Not Covered Points` Link: https://atcoder.jp/contests/abc462/tasks/abc462_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc462/submissions/76642849

---
+++ submissions/atcoder/abc462/abc462C Not_Covered_Points.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define v vector
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define f(arr, n) for(ll i = 0; i < n; i++) cin >> arr[i];
+
+const ll MOD = 998244353;
+const ll MAXD = 1000005;
+
+vector<ll> sieve(ll sieve_size)
+{
+    vector<ll> s(sieve_size + 1, 1);
+    s[0] = s[1] = 0;  
+
+    for (ll i = 2; i * i <= sieve_size; i++)
+    {
+        if (s[i] == 1)
+        {
+            for (ll j = i * i; j <= sieve_size; j += i)  
+            {
+                s[j] = 0;
+            }
+        }
+    }
+    return s;
+}
+int main()
+{   
+    fast_io;
+    ll n;
+    cin>>n;
+    v<pair<ll,ll>>points;
+    ll z=n;
+    while(z--)
+    {
+        ll x,y;
+        cin>>x>>y;
+        points.pb({x,y});
+    }
+    sort(all(points));
+    ll ans=0;
+    ll mn=LLONG_MAX;
+    for(auto it:points)
+
+{
+    ll x=it.first;
+    ll y=it.second;
+    if(y<mn)
+    {
+        ans++;
+        mn=y;
+    }
+}    
+cout<<ans<<endl;
+}
