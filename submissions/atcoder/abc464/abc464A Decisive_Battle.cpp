b8dc7d64b70f32952bf438618e5426cfa7e5d318
Abhiraj Mandal <mandalabhiraj04@gmail.com>
2026-06-27 18:01:00 +0600

    Add solution for problem `A - Decisive Battle` Link: https://atcoder.jp/contests/abc464/tasks/abc464_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc464/submissions/76989551

---
+++ submissions/atcoder/abc464/abc464A Decisive_Battle.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define v vector
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define f(arr, n) for(ll i = 0; i < n; i++) cin >> arr[i];
+
+const ll MOD = 998244353;
+const ll MAXD = 1000005;
+
+vector<ll> sieve(ll sieve_size)
+{
+    vector<ll> s(sieve_size + 1, 1);
+    s[0] = s[1] = 0;  
+
+    for (ll i = 2; i * i <= sieve_size; i++)
+    {
+        if (s[i] == 1)
+        {
+            for (ll j = i * i; j <= sieve_size; j += i)  
+            {
+                s[j] = 0;
+            }
+        }
+    }
+    return s;
+}
+int main()
+{   
+    fast_io;
+    string s;
+    cin>>s;
+    unordered_map<char,ll>mpp;
+    for(char c:s)
+    {
+        mpp[c]++;
+    }
+    if(mpp['E']>mpp['W'])
+    {
+        cout<<"East"<<endl;
+    }
+    else
+    {
+         cout<<"West"<<endl;
+    }
+}
